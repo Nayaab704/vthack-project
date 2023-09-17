6e22967735dcccbb634cddea425df3c36cda102c
Nayaab704 <nayaabshaikh704@gmail.com>
2023-09-17 19:24:25 +0530

    Header and styling

--- crime-notify/src/Components/Header.js
+++ crime-notify/src/Components/Header.js
@@ -4,11 +4,47 @@ import React from 'react';
 function Header() {
   return (
     <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-              <h2>VTHacks Project: Crime Location Visualizer</h2>
+
+     
+        <nav class="navbar">
+
+          <div class="logo">MUO</div>
+
+
+          <ul class="nav-links">
+
+
+            
+
+
+            <div class="menu">
+
+              <li><a href="/">Home</a></li>
+              <li><a href="/">About</a></li>
+
+              <li class="services">
+                <a href="/">Services</a>
+
+
+                <ul class="dropdown">
+                  <li><a href="/">Dropdown 1 </a></li>
+                  <li><a href="/">Dropdown 2</a></li>
+                  <li><a href="/">Dropdown 2</a></li>
+                  <li><a href="/">Dropdown 3</a></li>
+                  <li><a href="/">Dropdown 4</a></li>
+                </ul>
+
+              </li>
+
+              
+              <li><a href="/">Contact Us</a></li>
             </div>
+          </ul>
         </nav>
+
+       
+      
+
     </>
   );
 }
